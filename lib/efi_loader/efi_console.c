da8364d584611eb947fb3b6aab3f7b2839902758
Eryk Wieliczko <eryk@wieliczko.ninja>
2026-09-02 11:14:19 +0200

    usb: remove experimental Apple DWC3 handoff

--- lib/efi_loader/efi_console.c
+++ lib/efi_loader/efi_console.c
@@ -19,7 +19,6 @@
 #include <log.h>
 #include <stdio_dev.h>
 #include <video_console.h>
-#include <usb/apple_dwc3_handoff.h>
 #include <linux/delay.h>
 
 #define EFI_COUT_MODE_2 2
@@ -1019,14 +1018,6 @@ static efi_status_t EFIAPI efi_cin_read_key_stroke_ex(
 
 	EFI_ENTRY("%p, %p", this, key_data);
 
-	/*
-	 * The inherited transport owns DWC3 event polling.  Until the Apple input
-	 * backends can be polled without re-entering that path, report no EFI key
-	 * rather than allowing timer-driven input to corrupt the handoff state.
-	 */
-	if (apple_dwc3_handoff_active())
-		return EFI_EXIT(EFI_NOT_READY);
-
 	/* Check parameters */
 	if (!this || !key_data) {
 		ret = EFI_INVALID_PARAMETER;
@@ -1299,8 +1290,7 @@ static void EFIAPI efi_console_timer_notify(struct efi_event *event,
 					    void *context)
 {
 	EFI_ENTRY("%p, %p", event, context);
-	if (!apple_dwc3_handoff_active())
-		efi_cin_check();
+	efi_cin_check();
 	EFI_EXIT(EFI_SUCCESS);
 }
 
